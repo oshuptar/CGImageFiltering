6df0f0219e35e26964eedab1744000d84653f810
Oleh Shuptar <01193038@pw.edu.pl>
2026-03-05 15:38:07 +0100

    first commit

---
+++ CGImageFiltering.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CGImageFiltering.App.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ CGImageFiltering.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CGImageFiltering.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
